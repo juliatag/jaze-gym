e8e2b8835c3b1ad6b7a2f94cd6e305c4df8ce850
karizmaj <julieta.jabbot@gmail.com>
2022-08-12 17:16:03 -0400

    Bmi Working
    but needs bounds and validation

--- scripts/tools.js
+++ scripts/tools.js
@@ -1,43 +1,46 @@
+let $age = $("#age");
+let $weight = $("#weight");
+let $height = $("#height");
 
+let baseUrl = `https://fitness-calculator.p.rapidapi.com/`;
 
-
-let age =  $('#age').val();
-let weight = $('#weight').val();
-let height = $('#height').val();
-
-
-
-
-let calc = "bmi";
-let requestUrl= `https://fitness-calculator.p.rapidapi.com/${calc}?age=${age}&weight=${weight}&height=${height}&waist`;
-
-//For BMI
-let bmi;
-let health = '';
+$("#submit").on("click", showBmi);
 
 //get elements
 
-
-
-
 const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": requestUrl,
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "b3fac6f21cmsh1046448cb17e58ap14a9f2jsn993b6500d6de",
-		"X-RapidAPI-Host": "fitness-calculator.p.rapidapi.com"
-	}
+  async: true,
+  crossDomain: true,
+  url: baseUrl,
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b3fac6f21cmsh1046448cb17e58ap14a9f2jsn993b6500d6de",
+    "X-RapidAPI-Host": "fitness-calculator.p.rapidapi.com",
+  },
 };
 
+function getBmiParams() {
+  let age = $age.val();
+  let weight = $weight.val();
+  let height = $height.val();
 
-$('#submit').on("click", calculateBmi);
+  let calc = "bmi";
+  let params = `${calc}?age=${age}&weight=${weight}&height=${height}&waist`;
 
-function calculateBmi() {
-$.ajax(settings).done(function (response) {
-console.log('test');
-	$('.result .bmi span').text(response.data.bmi);
-	$('.result .health span').text(response.data.health);
-});
+  return params;
 }
+
+
+
+function showBmi() {
+
+	let requestUrl = baseUrl + getBmiParams();
+	settings.url = requestUrl;
+
+	$.ajax(settings).done(function (response) {
+		$(".result .bmi span").text(response.data.bmi);
+		$(".result .health span").text(response.data.health);
+	  });
+
+}
+
