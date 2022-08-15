df1dc4b8e2f5f24f7f454f30a42ed064576bb075
karizmaj <julieta.jabbot@gmail.com>
2022-08-15 16:08:35 -0400

    combined 2 forms into one

--- scripts/addFooter.js
+++ scripts/addFooter.js
@@ -13,31 +13,20 @@ fetch("components/footer.html")
 function getFeedbackModal() {
   // Create variables for the feedback form buttons, the modal background, and the modal itself
 
-  const siteFeedBtn = document.querySelector("#siteFeedbackBtn");
-  const gymFeedBtn = document.querySelector("#gymFeedbackBtn");
-  const modalBgs = document.querySelectorAll(".modal-background");
-  const modalGym = document.querySelector("#modalGym");
-  const modalSite = document.querySelector("#modalSite");
 
- // console.log(siteFeedBtn, gymFeedBtn, modalBgs, modalGym, modalSite);
+  const feedbackBtn = document.querySelector("#feedbackBtn");
+  const modalBg = document.querySelector(".modal-background");
+  const modal = document.querySelector("#modal");
 
   // Add an onClick listener to Gym feedback button
-  gymFeedBtn.addEventListener("click", (e) => {
+  feedbackBtn.addEventListener("click", (e) => {
     // Display the feedbackform Modal
-    modalGym.classList.add("is-active");
-  });
-
-  // Add an onClick listener to Site feedback button
-  siteFeedBtn.addEventListener("click", (e) => {
-    // Display the feedbackform Modal
-    modalSite.classList.add("is-active");
+    modal.classList.add("is-active");
   });
 
   // Add an onClick Listener for the dark part of the modal
   // This will make the modal disappear
-  modalBgs.forEach(bg => bg.addEventListener("click", () => {
-    modalGym.classList.remove("is-active");
-    modalSite.classList.remove("is-active");
-
-  }));
+  modalBg.addEventListener("click", () => {
+    modal.classList.remove("is-active");
+  });
 }
