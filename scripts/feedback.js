585bc857214cf1e9d3125cd61ad8aab69ca8db73
aftab87 <0514314@johnabbottcollege.net>
2022-08-14 23:00:01 -0400

    Feedback now sends the Email

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -35,24 +35,6 @@ function onInput(e) {
 }
 
 /**
- * Check for validation and submit
- */
-function submit() {
-    const $email = document.querySelector('#form-feedback input.email');
-    const $msg = document.querySelector('#form-feedback textarea.msg');
-    if (!validate($email)) {
-        $email.focus();
-        $('.help.email').removeClass('is-hidden');
-    } else if (!validate($msg)) {
-        $msg.focus();
-        $('.help.msg').removeClass('is-hidden');
-    } else {
-        // Send email
-        const $name = document.querySelector('#form-feedback input.name');
-    }
-}
-
-/**
  * 
  * @param {Element} target 
  * @returns {Boolean}
@@ -82,4 +64,39 @@ function validate(target) {
             break;
     }
     return true;
+}
+
+/**
+ * Check for validation and submit
+ */
+function submit() {
+    const $email = document.querySelector('#form-feedback input.email');
+    const $msg = document.querySelector('#form-feedback textarea.msg');
+    if (!validate($email)) {
+        $email.focus();
+        $('.help.email').removeClass('is-hidden');
+    } else if (!validate($msg)) {
+        $msg.focus();
+        $('.help.msg').removeClass('is-hidden');
+    } else {
+        // Send email
+        const $name = document.querySelector('#form-feedback input.name');
+        const name = (($name.value) ? $name.value : 'Anonymous') + ' JAZE Feedback';
+        const email = ($email.value) ? $email.value : '';
+        const msg = $msg.value;
+        emailjs.init('RjfHFBbtlV7Yc4Ys8');
+        var templateParams = {
+            name: name,
+            email: email,
+            message: msg
+        };
+         
+        emailjs.send('service_8zps2hk', 'template_h17i7dh', templateParams)
+            .then(function(response) {
+               console.log('SUCCESS!', response.status, response.text);
+            }, function(error) {
+               console.log('FAILED...', error);
+            });
+        
+    }
 }
